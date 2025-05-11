81d0414c3d1d90227c66e59fcd4a21696ad4bad6
G0blain <user.goblain@gmail.com>
2025-05-11 17:08:17 +0200

    cropping page UI

--- lib/cropping_page.dart
+++ lib/cropping_page.dart
@@ -25,6 +25,99 @@ class _CroppingPageState extends State<CroppingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Crop image')),
+      body: Center(
+        child: Column(
+          children: [
+            Expanded(
+              child: Crop(
+                image: widget._imageBytes,
+                controller: _cropController,
+                onCropped: (result) {
+                  switch (result) {
+                    case CropSuccess(:final croppedImage):
+                      Navigator.pop(context, croppedImage);
+                      break;
+                    case CropFailure(:final cause):
+                      showDialog(
+                        context: context,
+                        builder:
+                            (context) => AlertDialog(
+                              title: Text('Error'),
+                              content: Text('Failed to crop image: $cause'),
+                              actions: [
+                                TextButton(
+                                  onPressed: () => Navigator.pop(context),
+                                  child: Text('OK'),
+                                ),
+                              ],
+                            ),
+                      );
+                      break;
+                  }
+                },
+                withCircleUi: _isCircleUI,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        _isCircleUI
+                            ? Colors.blue
+                            : Colors.blue.withAlpha((0.3 * 255).toInt()),
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 12,
+                    ),
+                  ),
+                  onPressed: () => _useCircleUI(true),
+                  icon: const Icon(Icons.circle_outlined),
+                  label: const Text('CIRCLE'),
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        !_isCircleUI
+                            ? Colors.blue
+                            : Colors.blue.withAlpha((0.3 * 255).toInt()),
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 12,
+                    ),
+                  ),
+                  onPressed: () => _useCircleUI(false),
+                  icon: const Icon(Icons.crop_square),
+                  label: const Text('RECT'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                _isCircleUI
+                    ? _cropController.cropCircle()
+                    : _cropController.crop();
+              },
+              child: Text('Validate'),
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+
+  /*
+    @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Crop image')),
       body: LayoutBuilder(
         builder: (context, constraints) {
           final double cropHeight = constraints.maxHeight * 0.6;
@@ -123,4 +216,5 @@ class _CroppingPageState extends State<CroppingPage> {
       ),
     );
   }
+   */
 }
